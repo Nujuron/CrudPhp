ceb47e31c7b6c005695982649a0c04c748ef26f4
Nujuron <adri.brito@outlook.es>
2021-10-22 12:50:18 +0100

    user controller

---
+++ includes/user.php
@@ -0,0 +1,40 @@
+<?php
+
+class User extends DB {
+
+    private $name;
+    private $last_name;
+    private $username;
+
+    public function userExists($user, $pass) {
+        $md5pass = md5($pass);
+
+        $query = $this->connect()->prepare('SELECT * FROM users WHERE user_name=:user AND user_pass=:pass');
+        $query->execute(['user'=> $user, 'pass'=> $md5pass]);
+
+        if ($query->rowCount()) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public function setUser($user) {
+        $query = $this->connect()->prepare('SELECT * FROM users WHERE user_name=:user');
+        $query->execute(['user'=> $user]);
+
+        foreach ($query as $currentUser) {
+            $this->name = $currentUser['name'];
+            $this->last_name = $currentUser['last_name'];
+            $this->username = $currentUser['user_name'];
+        }
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+}
+
+
+?>
