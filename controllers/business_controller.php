7e995e2c5d8d0f88c4723609d50f83592c774a0b
Nujuron <adri.brito@outlook.es>
2021-10-26 16:11:31 +0100

    Added id to query

--- controllers/business_controller.php
+++ controllers/business_controller.php
@@ -6,7 +6,7 @@ class BusinessController extends DB {
 
     public function getAllBusiness($userid) {
         $query = $this->connect()->prepare(
-            'SELECT b.business_name, b.vat_number, b.status, b.email, b.phone 
+            'SELECT b.id, b.business_name, b.vat_number, b.status, b.email, b.phone 
             FROM business b 
             INNER JOIN records r USING (id) 
             WHERE r.user_id=:user');
--- controllers/customer_controller.php
+++ controllers/customer_controller.php
@@ -6,7 +6,7 @@ class CustomerController extends DB {
 
     public function getAllBusiness($userid) {
         $query = $this->connect()->prepare(
-            'SELECT c.name, c.last_name, c.status, c.email, c.phone 
+            'SELECT c.id, c.name, c.last_name, c.status, c.email, c.phone 
             FROM customer c 
             INNER JOIN records r USING (id) 
             WHERE r.user_id=:user');
